5ea32dd207c9eda814d186a6c8be969e6f2bbc53
yg-kim-korean <54833991+yg-kim-korean@users.noreply.github.com>
2022-02-03 23:52:33 +0900

    Initial commit

---
+++ src/main/java/com/server/RoadToInerview/RoadToInerviewApplication.java
@@ -0,0 +1,13 @@
+package com.server.RoadToInerview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadToInerviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadToInerviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/RoadToInerview/RoadToInerviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.RoadToInerview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadToInerviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
